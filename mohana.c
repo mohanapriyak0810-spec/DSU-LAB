ea6e16ad3769eea81e238e5d536db36f52210721
mohanapriyak0810-spec <mohanapriyak0810@gmail.com>
2026-08-31 12:21:11 +0530

    Add files via upload

---
+++ mohana.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+void create(int n) {
+    struct node *p, *t;
+
+    while (n--) {
+        p = malloc(sizeof(*p));
+        scanf("%d", &p->data);
+        p->next = NULL;
+
+        if (!head)
+            head = p;
+        else {
+            t = head;
+            while (t->next)
+                t = t->next;
+            t->next = p;
+        }
+    }
+}
+void traverse() {
+    struct node *p = head;
+
+    while (p) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+void insert(int x) {
+    struct node *p = malloc(sizeof(*p));
+
+    p->data = x;
+    p->next = head;
+    head = p;
+}
+void delete() {
+    struct node *p = head;
+
+    if (head) {
+        head = head->next;
+        free(p);
+    }
+}
+void search(int x) {
+    struct node *p = head;
+
+    while (p) {
+        if (p->data == x) {
+            printf("Found");
+            return;
+        }
+        p = p->next;
+    }
+    printf("Not Found");
+}
+int main() {
+    create(22);
+    traverse();
+    insert(13);
+    delete();
+    search(22);
+}
