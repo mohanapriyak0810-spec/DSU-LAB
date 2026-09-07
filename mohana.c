223b0a63f793dca061b1b80a269e3c83004cf464
mohanapriyak0810-spec <mohanapriyak0810@gmail.com>
2026-09-07 12:26:32 +0530

    Add files via upload

--- mohana.c
+++ mohana.c
@@ -1,67 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct node {
     int data;
+    struct node *prev;
     struct node *next;
 };
-struct node *head = NULL;
-void create(int n) {
-    struct node *p, *t;
 
-    while (n--) {
-        p = malloc(sizeof(*p));
-        scanf("%d", &p->data);
-        p->next = NULL;
+void insert(struct node **head, int x, int pos) {
+    struct node *newnode = malloc(sizeof(struct node));
 
-        if (!head)
-            head = p;
-        else {
-            t = head;
-            while (t->next)
-                t = t->next;
-            t->next = p;
+    if (newnode == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+
+    newnode->data = x;
+
+    // Insert at position 1
+    if (pos == 1) {
+        newnode->prev = NULL;
+        newnode->next = *head;
+
+        if (*head != NULL) {
+            (*head)->prev = newnode;
         }
+
+        *head = newnode;
+        return;
     }
-}
-void traverse() {
-    struct node *p = head;
 
-    while (p) {
-        printf("%d ", p->data);
-        p = p->next;
+    // Insert at position > 1
+    struct node *temp = *head;
+
+    for (int i = 1; i < pos - 1 && temp != NULL; i++) {
+        temp = temp->next;
     }
-}
-void insert(int x) {
-    struct node *p = malloc(sizeof(*p));
 
-    p->data = x;
-    p->next = head;
-    head = p;
-}
-void delete() {
-    struct node *p = head;
+    // Invalid position
+    if (temp == NULL) {
+        free(newnode);
+        return;
+    }
 
-    if (head) {
+    newnode->next = temp->next;
+    newnode->prev = temp;
+
+    if (temp->next != NULL) {
+        temp->next->prev = newnode;
+    }
+
+    temp->next = newnode;
+}
+
+void display(struct node *head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
         head = head->next;
-        free(p);
     }
+    printf("\n");
 }
-void search(int x) {
-    struct node *p = head;
 
-    while (p) {
-        if (p->data == x) {
-            printf("Found");
-            return;
-        }
-        p = p->next;
-    }
-    printf("Not Found");
-}
 int main() {
-    create(22);
-    traverse();
-    insert(13);
-    delete();
-    search(22);
+    struct node *head = NULL;
+
+    insert(&head, 10, 1);
+    insert(&head, 20, 2);
+    insert(&head, 30, 3);
+
+    display(head);
+
+    printf("Code Completed!\n");
+
+    return 0;
 }
+
